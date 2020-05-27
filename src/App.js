3a11021041477b0a8e58e34737f87dcb87fee3dd
John Duong <jhnduong@gmail.com>
2020-05-27 15:37:21 -0700

    consolidate cards

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 import * as Text from './text';
 import HeaderComp from './HeaderComp'
+import Card from './Card'
 
 AOS.init();
 const imgBg = "https://news.sophos.com/wp-content/uploads/2019/07/shutterstock_571378933-compressor.jpg"
 function App() {
-  let titles = [
-    {name: Text.CLOUD_WEB_HOSTING, body: Text.CLOUD_WEB_HOSTING_D},
-    {name: Text.CLOUD_SERVER, body: Text.CLOUD_SERVER_D},
-    {name: Text.CLOUD_MAIL, body: Text.CLOUD_MAIL_D},
-    {name: Text.DOMAIN, body: Text.DOMAIN_D},
-    {name: Text.COLO, body: Text.COLO_D},
-    {name: Text.SSL, body: Text.SSL_D},
-    {name: Text.DED, body: Text.DED_D},
+  const titles = [
+    { name: Text.CLOUD_WEB_HOSTING, body: Text.CLOUD_WEB_HOSTING_D },
+    { name: Text.CLOUD_SERVER, body: Text.CLOUD_SERVER_D },
+    { name: Text.CLOUD_MAIL, body: Text.CLOUD_MAIL_D },
+    { name: Text.DOMAIN, body: Text.DOMAIN_D },
+    { name: Text.COLO, body: Text.COLO_D },
+    { name: Text.SSL, body: Text.SSL_D },
+    { name: Text.DED, body: Text.DED_D },
+  ]
+
+  const cards = [
+    { name: Text.CLOUD_WEB_HOSTING, li: Text.CWH_LI },
+    { name: Text.CLOUD_SERVER, li: Text.CS_LI },
+    { name: Text.CLOUD_MAIL, li: Text.CM_LI }
   ]
 
   return (
@@ -30,7 +37,7 @@ function App() {
           </div>
           <div className="flex-row">
             <ul className="tabs">
-              {titles.map((title) => {                
+              {titles.map((title) => {
                 return <HeaderComp name={title.name} body={title.body} />
               })}
             </ul>
@@ -39,52 +46,12 @@ function App() {
             <img data-aos="fade-left" src={imgBg} className="img-fluid main-image" />
           </div>
         </header>
-        <div className="row">
-          <div className="col-sm-4">
-            <div className="card">
-              <div className="card-body">
-                <h2 className="card-title">{Text.CLOUD_WEB_HOSTING}</h2>
-                <div className="card-text">
-                  <ul>
-                    {Text.CWH_LI.map(val => {
-                      return <li>{val}</li>
-                    })}
-                  </ul>
+        <div className="row">         
+            {cards.map(card => {
+              return <div className="col-sm-4">
+                <Card name={card.name} li={card.li} />
                 </div>
-                <a href="#" class="btn btn-primary">Learn more</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="card">
-              <div className="card-body">
-                <h2 className="card-title">{Text.CLOUD_SERVER}</h2>
-                <div className="card-text">
-                  <ul>
-                    {Text.CWH_LI.map(val => {
-                      return <li>{val}</li>
-                    })}
-                  </ul>
-                </div>
-                <a href="#" class="btn btn-primary">Learn more</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="card">
-              <div className="card-body">
-                <h2 className="card-title">{Text.CLOUD_MAIL}</h2>
-                <div className="card-text">
-                  <ul>
-                    {Text.CM_LI.map(val => {
-                      return <li>{val}</li>
-                    })}
-                  </ul>
-                </div>
-                <a href="#" class="btn btn-primary">Learn more</a>
-              </div>
-            </div>
-          </div>
+            })}
         </div>
       </div>
     </div>
---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card = props => {
+  return <div className="card">
+    <div className="card-body">
+      <h2 className="card-title">{props.name}</h2>
+      <div className="card-text">
+        <ul>
+          {props.li.map(val => {
+            return <li>{val}</li>
+          })}
+        </ul>
+      </div>
+      <a href="#" class="btn btn-primary">Learn more</a>
+    </div>
+  </div>
+}
+
+export default Card
