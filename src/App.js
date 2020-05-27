cc373bbe3ca48773eb3da045ab5794b619097ac6
John Duong <jhnduong@gmail.com>
2020-05-27 00:40:05 -0700

    update text rendering

--- src/App.js
+++ src/App.js
@@ -2,53 +2,57 @@ import React from 'react';
 import './App.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+import * as Text from './text';
+
 AOS.init();
+const imgBg = "https://www.dqindia.com/wp-content/uploads/2017/03/server_room.jpg"
 function App() {
   return (
-    <div className="container-fluid px-0">
+    <div className="d-flex justify-content-center">
       <div className="App">
         <header className="App-header">
           <div className="flex-row">
             <div className="App-logo col-md-6">
-              <img src="http://www.nineweb.co.th/images/nineweb_logo_blue.png" className="App-logo" alt="logo" />
-              <h1>Nine Web</h1>
+              <img src="nineweb_mod.png" className="App-logo" alt="logo" />
+              <h1>{Text.NINE_WEB}</h1>
             </div>
           </div>
           <div className="flex-row">
             <ul className="tabs">
-              <li className="dropdown">Cloud Web Hosting
-              <p className="dropdown-content">
-                  บริการเว็บโฮสติ้งบนระบบคลาวด์
-                  จุดเด่นของคลาวด์คือระบบจะย้ายเว็บไปยัง server ที่ทำให้เว็บเร็วที่สุดโดยอัตโนมัติ หรือหาก server
-                  มีปัญหา ระบบก็จะย้ายเว็บไปเครื่องอื่นโดยอัตโนมัติ โดยไม่มี down-time หรือหาก load บน server
-                  เริ่มมีมากขึ้นก็จะย้ายไป server ที่ load น้อยกว่าโดยอัตโนมัติเช่นกัน
-              </p>
+              <li className="dropdown">{Text.CLOUD_WEB_HOSTING}
+                <p className="dropdown-content">
+                  {Text.CLOUD_WEB_HOSTING_D}
+                </p>
               </li>
-              <li className="dropdown">Cloud Server (VPS)
-              <p className="dropdown-content">TEST
+              <li className="dropdown">{Text.CLOUD_SERVER}
+              <p className="dropdown-content">{Text.CLOUD_SERVER_D}
               </p></li>
-              <li>Cloud Mail Hosting</li>
-              <li>Domain</li>
-              <li>Colocation</li>
-              <li>SSL</li>
-              <li>Dedicated</li>
+              <li className="dropdown">{Text.CLOUD_MAIL}
+              <p className="dropdown-content">{Text.CLOUD_MAIL_D}
+              </p></li>
+              <li className="dropdown">{Text.DOMAIN}
+              <p className="dropdown-content">{Text.DOMAIN_D}
+              </p></li>
+              <li className="dropdown">{Text.COLO}
+              <p className="dropdown-content">{Text.COLO_D}
+              </p></li>
+              <li className="dropdown">{Text.SSL}
+              <p className="dropdown-content">{Text.SSL_D}
+              </p></li>
+              <li className="dropdown">{Text.DED}
+              <p className="dropdown-content">{Text.DED_D}
+              </p></li>
             </ul>
           </div>
-          <img src="https://www.dqindia.com/wp-content/uploads/2017/03/server_room.jpg" className="main-image" />
+          <img data-aos="fade-left" src={imgBg} className="main-image" />
         </header>
         <div data-aos="fade-in">
-          <h2>Cloud Web Hosting</h2>
-          <div className="row">
+          <h2>{Text.CLOUD_WEB_HOSTING}</h2>
+          <div className="flex-row">
             <ul>
-              <li>เว็บไซต์บน Cloud โหลดเร็วกว่า Hosting แบบเดิม ๆ</li>
-              <li>0% Down-time จากการซ่อมหรืออัพเกรด Hardware</li>
-              <li>เว็บไม่อืดด้วยระบบกระจายโหลดไป server อื่นโดยอัตโนมัติ</li>
-              <li>Reboot อัตโนมัติ เมื่อพบว่า OS ไม่ตอบสนอง</li>
-              <li>ใช้ Multipath SAN Storage และ SAS HDD</li>
-              <li>Redundant Cisco Switch และ Network Path</li>
-              <li>.NET 4, PHP 7, ASP 3, MSSQL 2017, MySQL 5</li>
-              <li>คืนเงินโดยไม่มีเงื่อนไข เมื่อยกเลิกภายใน 30 วัน</li>
+              {Text.CWH_LI.map(val => {
+                return <li>{val}</li>
+              })}
             </ul>
           </div>
         </div>
---
+++ src/text.js
@@ -0,0 +1,37 @@
+export const NINE_WEB = "Nine Web"
+export const CLOUD_WEB_HOSTING = "Cloud Web Hosting"
+export const CLOUD_WEB_HOSTING_D = `Major benefit of cloud is it will move your 
+  website to the fastest server available automatically. 
+  In contrary to traditional hosting, your site stays on a server 
+  it was setup on forever until there is something happens to that server. 
+  On cloud, you will never notice when your site is moved. 
+  It is moved as soon as the cloud detects load imbalance.`
+export const CLOUD_SERVER = "Cloud Server (VPS)"
+export const CLOUD_SERVER_D = `You can specify your server specification 
+such as CPU (GHz), Memory (GB) and Disk space (GB). They can be adjusted 
+at any time. You only pay for what you really need.
+Getting your own hardware is quite a risk when you need to upgrade. 
+It may require minutes of down-time and usually need days or weeks to order the parts. 
+This is totally different in Cloud technology. The setup may take upto only a few hours 
+and an upgrade can be done with no down-time or under a minute; virtually no 
+down-time in most cases.`
+export const CLOUD_MAIL = "Cloud Mail Hosting"
+export const CLOUD_MAIL_D = `Mail Service on Cloud tremendously increases traffic handling capability compared to regular mail server. Thanks to Distributed Resource Scheduler (DRS) on Cloud which prevents any single erver to get overloaded. Moreover, at Nine Web, we deploy multiple outgoing mail gateways to dispatch mails quicker and to help reducing blacklisting when attacked by spams.`
+export const DOMAIN = "Domain"
+export const DOMAIN_D = `Nine Web is an authorized service providers in registering many TLDs around the world such as .COM .NET .ORG .TH .BIZ .INFO and many more. Tens of thousands domain names have been registered with us.`
+export const COLO = `Co-location`
+export const COLO_D = `We have 2 Data Centers in Bangkok's CBD which is the first Green IDC in Thailand. The centers are designed to run 24x7 even in flood situation with N+1 generators on 6th floor. With one of the largest bandwidth capacity for business use, we deliver your traffic at the maximum speed possible; upto 22Gbps domestic and 1Gbps international backbone, 4th largest in Thailand.`
+export const SSL = `SSL`
+export const SSL_D = `We offer SSL Certificate from 3 major CAs; Thawte, Verisign and GeoTrust for every level from personal websites to enterprise multiple servers. Price starts from 2,800 Baht/year.`
+export const DED = `Dedicated`
+export const DED_D = `For enterprises or individuals who need high level of performance and/or special configuration in a server, dedicated server (lease or hire-purchase) is the best option to meet your needs. We can acquire and configure hardware from many brands such as Dell, HP, IBM, Cisco, Intel, etc. for you.`
+
+export const CWH_LI = [
+  "Web on Cloud is faster than regular hosting",
+"0% Hardware Down-time from harware MA",
+"Best response time by resource distribution",
+"Automatic reboot when OS not response",
+"Use Multipath SAN Storage and SAS HDD",
+"Redundant Cisco Switch และ Network Path",
+".NET 4, PHP 5, ASP 3, MSSQL 2012, MySQL 5",
+"30-day unconditional money-back guarantee"]
